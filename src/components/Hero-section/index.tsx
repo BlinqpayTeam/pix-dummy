f7baf038a8acd2c0d88764178442b6dd2d60ed42
User <user@Users-MacBook-Pro.local>
2024-12-24 01:57:25 +0100

    first commit

---
+++ src/components/Hero-section/index.tsx
@@ -0,0 +1,48 @@
+import { cn } from '@/lib/utils'
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from '../ui/navigation-menu'
+import Image from 'next/image'
+
+type Props = {
+    menus: Array<string>
+}
+
+const HeroSectionMenu = ({menus}: Props) => {
+  return (
+      <div className='flex justify-between items-center px-20 py-10'>
+          <Image src="/logo/blinqpay.svg" alt="logo" width={82} height={18} />
+          <NavigationMenu>
+              <NavigationMenuList>
+                  {menus.map(menu => (
+                      menu.toLowerCase() === "products" ? <NavigationMenuItem key={menu} className='text-white bg-transparent  hover:bg-transparent'>
+                          <NavigationMenuTrigger className='bg-transparent !hover:bg-transparent'>{menu}</NavigationMenuTrigger>
+                          <NavigationMenuContent>
+            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                   hello
+                  </a>
+                </NavigationMenuLink>
+              </li>
+             
+            </ul>
+          </NavigationMenuContent>
+                          </NavigationMenuItem>
+                          : <NavigationMenuItem className={cn('text-white', {
+                                  " !mr-4": menu.toLowerCase() === 'developers'
+                              })} key={menu}>{menu}</NavigationMenuItem>
+                      
+                  ))}
+       
+      </NavigationMenuList>
+          </NavigationMenu>
+          
+        <div/>
+    </div>
+  )
+}
+
+export default HeroSectionMenu
---
+++ src/components/Hero-section/section.tsx
@@ -0,0 +1,22 @@
+import { Button } from "../ui/button";
+
+const HeroSection = () => {
+    return (
+        <div className="text-white">
+            <div className=" w-[45%] mx-auto text-center">
+                <div className="text-sm font-medium flex justify-center mb-3">
+                        <img src="/images/star.svg" alt="star" width={12} height={12} />
+                    <p className="ml-2">One platform, multiple solutions</p></div>
+            <p className="text-[80px] font-bold leading-[1.2] mb-3">CONNECTING YOU TO THE WORLD.</p>
+            <p className="w-[75%] mx-auto text-base">Your gateway to global payments, ecommerce integration, account management,
+                and multiple payment channels.</p>
+            <Button className="text-base font-normal bg-mediumBlue h-[48px] text-sm font-medium mt-10">Explore our Products</Button>
+           </div>
+            <div>
+            <img src="/images/world.svg" alt="world" className="w-[100vw]" />
+           </div>
+        </div>
+    )
+}
+
+export default HeroSection;
