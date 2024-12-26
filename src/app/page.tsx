f9b700b0440c57249aa84bdda40bf2fd481b6c2a
User <user@Users-MacBook-Pro.local>
2024-12-26 12:14:15 +0100

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Banner from "@/components/Banner";
 import HeroSectionMenu from "@/components/Hero-section";
 import HeroSection from "@/components/Hero-section/section";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import SecondHalfSectionsWrapper from "@/components/landing-page/second-half-sections/SecondHalfSectionsWrapper";
 =======
 <<<<<<< HEAD
@@ -10,6 +11,9 @@ import SecondHalfSectionsWrapper from "@/components/second-half-sections/SecondH
 import Image from "next/image";
 >>>>>>> 5abec7e (update)
 >>>>>>> ea67848 (firs)
+=======
+import SecondHalfSectionsWrapper from "@/components/second-half-sections/SecondHalfSectionsWrapper";
+>>>>>>> 665ebe4 (firs)
 
 const menu = ["Company", "Products", "Developers", "About us"]
 
