ca7088880165f28d23b521ebd290c0f713eb5e36
AyoTheInspired <ayoinspired@gmail.com>
2024-12-27 13:44:50 +0100

    Setup checkout section

---
+++ src/components/landing-page/second-half-sections/CheckoutSection.tsx
@@ -0,0 +1,38 @@
+'use client';
+import SectionLabel from '@/components/ui/SectionLabel'
+import BlinqButton from '@/components/ui/BlinqButton';
+import { ChevronRight } from 'lucide-react';
+
+const CheckoutSection = () => {
+
+   return (
+      <div className='bg-blg-dark-200 text-white pb-20 pt-14 flex items-center justify-center flex-col side-pad'>
+
+         <SectionLabel text="Blinq Checkout" />
+
+         <p className="my-5 lg:my-7 text-3xl leading-10 lg:leading-[50px] lg:text-[38px] font-medium text-center">
+            Accept Payments Globally
+         </p>
+
+         <div className="text-blq-gray-300 text-plg lg:text-slg mb-8 lg:mb-12 text-center lg:w-2/5">
+            Whether you are a small business owner, an NGO, a start-up, or multinational, acept payments in multiple currencies wherever you are in the world with our smart checkout system.
+         </div>
+
+         <div className="flexed">
+            <BlinqButton
+               extraPadding
+               text="Sign up"
+            />
+
+            <a href='#' target="_blank" rel="noreferrer" className='text-white ml-5 flex items-center text-sm lg:text-base'>
+               Learn more&nbsp;
+               <span>
+                  <ChevronRight size="1rem" />
+               </span>
+            </a>
+         </div>
+      </div>
+   )
+}
+
+export default CheckoutSection
--- src/components/landing-page/second-half-sections/ZapSection.tsx
+++ src/components/landing-page/second-half-sections/ZapSection.tsx
@@ -32,7 +32,7 @@ const ZapSection = () => {
    ]
 
    return (
-      <div className="w-full zap-section-overlay bred relative lg:bg-contain bg-no-repeat text-white">
+      <div className="w-full zap-section-overlay relative lg:bg-contain bg-no-repeat text-white">
 
          <div className="absolute top-20 lg:top-28 w-full mx-auto flexed flex-col px-4 lg:px-0">
             <SectionLabel text="ZAP" />
