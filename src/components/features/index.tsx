d5ff90af9f743d21084f6e8e520675a6fd24d23f
User <user@Users-MacBook-Pro.local>
2024-12-29 22:37:40 +0100

    fix updated

--- src/components/features/index.tsx
+++ src/components/features/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Features = () => {
   return (
-      <div className='text-center text-white pt-52 overflow-hidden'>
+      <div className='text-center text-white pt-20 overflow-hidden'>
           <div className='text-white bg-cardBg rounded-3xl flex justify-center items-center w-[62px] mb-8 h-[36px] mx-auto'>Qwid</div>
           <p className='text-white lg:text-[40px] text-[32px] font-medium w-[70%] lg:w-[20%] mx-auto'>Cross Border Payment Solution.</p>
           <p className='text-textColor lg:w-[23%] w-[90%] mt-2 mx-auto'>Make payments with multiple currencies across multiple countries, convert, hold, send and receive funds across borders in a few clicks.</p>
@@ -12,7 +12,7 @@ const Features = () => {
               <p>visit our website</p>
               <Image src="/images/chevron-right.svg" alt="chevron" width={14} height={14} />
           </div>
-          <div className="flex justify-center items-center mb-20 mt-40 relative bg-[url('/images/feature.svg')] h-[520px] bg-no-repeat bg-center bg-cover lg:bg-contain">
+          <div className="flex justify-center items-center mb-20 mt-20 relative bg-[url('/images/feature.svg')] h-[520px] bg-no-repeat bg-center bg-cover lg:bg-contain">
               {/* <Image src="/images/feature.svg" alt="feature" height={520} width={580} /> */}
               <div className='absolute h-[10px] w-full bottom-0 shadow-[0_0px_50px_70px_rgba(1,1,15,1)] border-solid border bg-darkBlue border-darkBlue'/>
           </div>
--- src/components/frames/index.tsx
+++ src/components/frames/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Frames = () => {
   return (
-      <div className="flex flex-wrap lg:grid lg:grid-rows-2 grid-flow-col lg:gap-2 lg:w-[75%] mx-6 lg:mx-auto my-20">
+      <div className="flex flex-wrap lg:grid lg:grid-rows-2 grid-flow-col lg:gap-2 lg:w-[75%] mx-6 lg:mx-auto mb-5">
         <Image loading="lazy" src="/images/features/multiple-curriencies.svg" alt="multiple curriencies" width={389} height={304} />
         <Image loading="lazy" src="/images/features/transaction-confirmation.svg" alt="transaction confirmation" width={389} height={304} />
         <Image loading="lazy" src="/images/features/free-transfers.svg" alt="free transfers" width={389} height={304} />
