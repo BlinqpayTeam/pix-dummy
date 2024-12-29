20fd295500ef4401ae312cbc08a0c7f3fbf80262
User <user@Users-MacBook-Pro.local>
2024-12-29 22:56:52 +0100

    fix updated

--- src/components/frames/index.tsx
+++ src/components/frames/index.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 const Frames = () => {
   return (
       <div className="flex flex-wrap lg:grid lg:grid-rows-2 grid-flow-col lg:gap-2 lg:w-[75%] mx-6 lg:mx-auto mb-5">
-        <Image loading="lazy" src="/images/features/multiple-curriencies.svg" alt="multiple curriencies" width={389} height={304} />
-        <Image loading="lazy" src="/images/features/transaction-confirmation.svg" alt="transaction confirmation" width={389} height={304} />
-        <Image loading="lazy" src="/images/features/free-transfers.svg" alt="free transfers" width={389} height={304} />
-        <Image loading="lazy" src="/images/features/exchange-rates.svg" alt="exchange rate" width={389} height={304} />
-        <Image loading="lazy" src="/images/features/transparent-fees.svg" alt="transparent fees" width={389} height={304} />
-        <Image loading="lazy" src="/images/features/accessibility.svg" alt="accessiblity" width={389} height={304} />
+        <Image loading="lazy" src="/images/features/multiple-curriencies.png" alt="multiple curriencies" width={389} height={304} />
+        <Image loading="lazy" src="/images/features/transaction-confirmation.png" alt="transaction confirmation" width={389} height={304} />
+        <Image loading="lazy" src="/images/features/free-transfers.png" alt="free transfers" width={389} height={304} />
+        <Image loading="lazy" src="/images/features/exchange-rates.png" alt="exchange rate" width={389} height={304} />
+        <Image loading="lazy" src="/images/features/transparent-fees.png" alt="transparent fees" width={389} height={304} />
+        <Image loading="lazy" src="/images/features/accessibility.png" alt="accessiblity" width={389} height={304} />
     </div>
   )
 }
